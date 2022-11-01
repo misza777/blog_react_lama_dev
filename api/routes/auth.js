256df91aeb64b6ff1443d06eb3fda67b00ea6bda
unknown <miau.nowak@gmail.com>
2022-11-01 13:36:28 +0200

    logi user and bcrypt

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,16 +1,38 @@
-const router= require('express').Router();
-
-const User = require('../models/Users');
+const router = require("express").Router();
+const User = require("../models/Users");
+const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req, res) => {
+  try {
+    // hidding password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
     try {
-        const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        });
-        const user = await newUser.save();
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(404).json("User not found!");
+    
+        const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+        );
+        !validPassword && res.status(400).json("Wrong password!");
+    
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json(error);
